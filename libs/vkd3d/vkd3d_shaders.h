4bb38b7585a8d798b7870d754ca82795b1d99eff
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-05-01 15:54:21 +0200

    vkd3d: Add simple debug ring meta shader for execute indirect.
    When using breadcrumbs_trace_indirect, allow generic indirect commands
    to go through debugger as well.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/vkd3d_shaders.h
+++ libs/vkd3d/vkd3d_shaders.h
@@ -47,6 +47,7 @@ enum vkd3d_meta_copy_mode
 #include <cs_resolve_query.h>
 #include <cs_execute_indirect_patch.h>
 #include <cs_execute_indirect_patch_debug_ring.h>
+#include <cs_execute_indirect_debug_ring.h>
 #include <cs_execute_indirect_multi_dispatch.h>
 #include <cs_execute_indirect_multi_dispatch_state.h>
 #include <vs_fullscreen_layer.h>
